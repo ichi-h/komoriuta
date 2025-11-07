b2d1412f410a37e0f11600ff7c142ebf9d7e25fb
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-11-07 23:09:45 +0900

    Create flake.nix temporary.

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    mcp-servers-nix = {
+      url = "github:natsukium/mcp-servers-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, mcp-servers-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        packages = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          default = packages.mkShell {
+            shellHook = let
+              config = mcp-servers-nix.lib.mkConfig packages {
+                programs = {
+                  serena.enable = true;
+                };
+              };
+            in ''
+              if [ -L ".mcp.json" ]; then
+                unlink .mcp.json
+              fi
+              ln -sf ${config} .mcp.json
+            '';
+          };
+        };
+      }
+    );
+}
